1e178467b2a79d02da8a0aae67c58d46d7fa0ad1
Denis <denvasilev1982@icloud.com>
2022-12-04 20:49:09 -0800

    Home Work6

---
+++ seminar 1/HW Seminar1/task6HW/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string? answer = Console.ReadLine();
+int A = Convert.ToInt32(answer);
+if (A % 2==1)
+{
+    Console.WriteLine("Число нечетное");
+}
+else 
+{
+    Console.WriteLine("Число четное");
+}
