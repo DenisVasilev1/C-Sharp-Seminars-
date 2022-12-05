a2e6dd862310492703f7b32d1344fb3d7581cc48
Denis <denvasilev1982@icloud.com>
2022-12-04 20:26:21 -0800

    Home Work2

---
+++ seminar 1/HW Seminar1/task2HW/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число 1: ");
+string? answer1 = Console.ReadLine();
+int numberA = Convert.ToInt32(answer1);
+
+Console.WriteLine("Введите число 2: ");
+string? answer2 = Console.ReadLine();
+int numberB = Convert.ToInt32(answer2);
+
+if (numberA > numberB)
+{
+    Console.Write("Большее - ");
+    Console.Write(numberA);
+    Console.WriteLine (" ");
+    Console.Write("Меньшее - ");
+    Console.Write(numberB);
+}
+else
+{
+    Console.Write("Большее - ");
+    Console.Write(numberB);
+    Console.WriteLine (" ");
+    Console.Write("Меньшее - ");
+    Console.Write(numberA);
+}
