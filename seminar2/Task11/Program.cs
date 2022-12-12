d5d2449cb59de332869189b42c7dfa1138f02e1f
Denis <denvasilev1982@icloud.com>
2022-12-11 17:26:03 -0800

    new task11

---
+++ seminar2/Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишиие программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа
+// например
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int digit2 = number % 10;
+int digit1 = number / 100;
+Console.WriteLine($"Искомое число: {digit1}{digit2}");
+//if (digit1 > digit2)
+//{
+//   Console.WriteLine(digit1);
+//}
+//else
+//{
+//    Console.WriteLine(digit2);
+//}
