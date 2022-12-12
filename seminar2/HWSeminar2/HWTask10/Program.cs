26681162aec55697d20e7cf32e2ebf28d12a5222
Denis <denvasilev1982@icloud.com>
2022-12-11 18:42:20 -0800

    hw seminar2 task10

---
+++ seminar2/HWSeminar2/HWTask10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и на 
+// выходе показывает вторую цифру этого числа
+// 124 -> 2;
+// 432 -> 3
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int digit1 = number / 10;
+int digit2 = digit1 / 10;
+int digit3 = digit1 - (digit2 * 10);
+
+Console.WriteLine($"Искомое число: {digit3}");
+
--- seminar2/Task16/Program.cs
+++ seminar2/Task16/Program.cs
@@ -8,14 +8,14 @@ Console.WriteLine("Введите число - ");
 int digit1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число - ");
-int digit2 = Convert.ToInt32(Console.ReadLine());
+int digit2 = Convert.ToInt32(Console.ReadLine()); 
 
 
 if (digit2 == digit1 * digit1 || digit1 == digit2 * digit2)
 {
     Console.WriteLine("Да");
 }
-
+ 
 else
 {
     Console.WriteLine("Нет");
