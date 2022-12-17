bad7874a0461f410f903989c23f0fe39c635a283
Denis <denvasilev1982@icloud.com>
2022-12-17 15:13:08 -0800

    Семинар 3 задача 18

---
+++ seminar3/task18/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает
+// диапозон возможных координат точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите четверть: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1)
+ {
+    Console.WriteLine("x < 0 и y > 0");
+ }
+
+else if (a == 2)
+ {
+    Console.WriteLine("x < 0 и y > 0");
+ }
+
+else if (a == 3)
+ {
+    Console.WriteLine("x < 0 и y < 0");
+ }
+
+else if (a == 4)
+ {
+    Console.WriteLine("x > 0 и y < 0");
+ }
+
+ else 
+{ 
+    Console.WriteLine("Такой четверти не существует");
+} 
