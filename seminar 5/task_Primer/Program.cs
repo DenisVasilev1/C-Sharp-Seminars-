69e4b74a1dac14938d5abf1a81608bc1ffb3a3f5
Denis <denvasilev1982@icloud.com>
2023-01-02 14:54:30 -0800

    seminar 5 task abbout massiv

---
+++ seminar 5/task_Primer/Program.cs
@@ -0,0 +1,18 @@
+﻿// как сравнить группу чисел массива
+
+
+
+int[] a = {1, 2, 3, 4, 5};
+int[] b = {1, 2, 3, 4, 5};
+
+bool res = true; // res = true то массивы равны, если res = false, то массивы не равны
+
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] != b[i])
+    {
+        res = false;
+    }
+}
+
+Console.WriteLine(res);
