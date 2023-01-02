ec8aa6765af4d2b52f4be18ae3a154182a98dc20
Denis <denvasilev1982@icloud.com>
2023-01-02 15:02:34 -0800

    seminar 5 task abbout massiv - уточнил условия задачи и поработали с одним массивом

--- seminar 5/task_Primer/Program.cs
+++ seminar 5/task_Primer/Program.cs
@@ -2,14 +2,14 @@
 
 
 
-int[] a = {1, 2, 3, 4, 5};
-int[] b = {1, 2, 3, 4, 5};
+int[] a = {1, 2, 3, 2, 1};
+//int[] b = {1, 2, 3, 4, 5};
 
 bool res = true; // res = true то массивы равны, если res = false, то массивы не равны
 
-for (int i = 0; i < a.Length; i++)
+for (int i = 0; i < a.Length / 2; i++)
 {
-    if (a[i] != b[i])
+    if (a[i] != a[a.Length - 1 - i])
     {
         res = false;
     }
