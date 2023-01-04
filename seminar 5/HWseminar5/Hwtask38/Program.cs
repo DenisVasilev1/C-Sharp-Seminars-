b413365099c90fa580da39189e6b9606da1621c2
Denis <denvasilev1982@icloud.com>
2023-01-03 19:26:40 -0800

    seminar 5 HWtask 38

---
+++ seminar 5/HWseminar5/Hwtask38/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+int[] GetRandomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+const int SIZE = 20;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 100;
+
+
+int[] arr = GetRandomArray(SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine(string.Join(", ", arr));
+
+int max = LEFTRANGE;
+int min = RIGHTRANGE;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+}
+
+
+Console.WriteLine($"максимальный элемент массива - {max}");
+Console.WriteLine($"минимальный элемент массива - {min}");
+Console.WriteLine($"разница между максимальным и минимальным элементами массива - {max-min}");
