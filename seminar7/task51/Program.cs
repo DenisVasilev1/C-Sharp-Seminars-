5ded05c8db4b07b4e165705f8bd943ad051cfc5c
Denis <denvasilev1982@icloud.com>
2023-01-05 15:53:09 -0800

    task 51 нахождение суммы элементов двумерного массива находящихся на главной диагонали

---
+++ seminar7/task51/Program.cs
@@ -0,0 +1,61 @@
+﻿//  Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+// Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может
+// быть прямоугольный
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 50)
+{
+    int[,] matr = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Task(int[,] matr)
+{
+    int sum = 0;
+    int lenght = 0;
+    if (matr.GetLength(0) > matr.GetLength(1))
+    {
+        lenght = matr.GetLength(1);
+    }
+    else
+    {
+        lenght = matr.GetLength(0);
+    }
+    for (int i = 0; i < lenght; i++)
+    {
+        sum += matr[i, i];
+    }
+    return sum;
+}
+
+int[,] matr = GetMatrix(4, 5, 3, 9);
+PrintMatrix(matr);
+int sum = Task(matr);
+Console.WriteLine();
+Console.WriteLine(sum);
