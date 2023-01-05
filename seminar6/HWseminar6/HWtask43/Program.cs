141fa3b22d9e8845f9fe7715e00fd7106411af22
Denis <denvasilev1982@icloud.com>
2023-01-05 14:25:08 -0800

    конвертация

--- seminar6/HWseminar6/HWtask43/Program.cs
+++ seminar6/HWseminar6/HWtask43/Program.cs
@@ -6,10 +6,10 @@
 
 Console.WriteLine("Введите k1 ");
 double k1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите k2 ");
-double k2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите b1 ");
 double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2 ");
+double k2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите b2 ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
---
+++ seminar7/task_primer/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+// int number = Convert.ToInt32("абракадабра");
+
+
+//int number = int.TryParse();
+//double floatnumber = double.TryParse();
+
+
+string str = "1213";
+bool res = int.TryParse(str, out int result);
+if (res == true)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("конвертация не произошла");
+}
+
+
