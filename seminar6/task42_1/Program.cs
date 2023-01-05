e76d50fa1ac8e896545de017a9870d6c7c934e08
Denis <denvasilev1982@icloud.com>
2023-01-04 19:08:58 -0800

    task42-1 перевод десятичного числа в двоичное через рекурсию

---
+++ seminar6/task42_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+
+void PrintBinaryView(int number)
+{
+    if (number <= 0) return;
+    PrintBinaryView(number / 2);
+    Console.Write(number % 2);
+}
+
+int a = 13;
+PrintBinaryView(a);
